de599a779ccdc2368dfd18b4c3be35dea308d086
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-19 15:29:13 +0530

    added functions to compute page number

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 //  console.log(itemList)
 
 const navItemOptions = {
-  template: `<li v-on:click="$emit('disp', $event)"><a href='#'>{{ item }}</a></li>`,
+  template: `<li v-on:click="$emit('story', $event)"><a href='#'>{{ item }}</a></li>`,
   props: ['item']
 }
 
 const navOptions = {
   el: '#nav',
   data: {
-    story: '',
+    story: 'top',
     navItems: ['TOP', 'NEW', 'BEST', 'SHOW', 'ASK', 'JOBS']
   },
   methods: {
-    display: function (e) {
-      console.log(e.target.text)
+    setStory: function (e) {
+      this.story = e.target.text.toLowerCase()
+      main.story = this.story
     }
   }
 }
@@ -21,10 +22,21 @@ const navOptions = {
 const mainOptions = {
   el: '#main',
   data: {
-    count: 1,
-    story: 'top'
+    pgno: 1,
+    story: 'top',
+    disable: true
   },
+  methods: {
+    alterPgno: function (e) {
+      if (e.target.textContent === 'Prev') {
+        this.pgno--
+      } else {
+        this.pgno++
+      }
+    }
+  }
 }
 
 Vue.component('nav-items', navItemOptions)
 const nav = new Vue(navOptions)
+const main = new Vue(mainOptions)
