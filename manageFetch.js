6c6a346990ca12d4677ed78860265df899292ffc
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-20 07:50:59 +0530

    remove useless functions

--- manageFetch.js
+++ manageFetch.js
@@ -12,17 +12,9 @@ const itemList = []
 const url = 'https://hacker-news.firebaseio.com/v0/'
 
 for (let i in urls) {
-  itemList.push({ [i]: fetch(`${url}${urls[i]}`).then(toJSON).catch(handleErr) })
+  itemList.push({ [i]: fetch(`${url}${urls[i]}`).then(toJSON).catch(console.error) })
 }
 
 function toJSON (data) {
   return data.json()
-}
-
-function handleErr (err) {
-  console.error(err)
-}
-
-function pushEach (list) {
-  // stories.items.push(list)
 }
