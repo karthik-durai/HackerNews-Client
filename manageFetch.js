47ab4478ff3a06fb683e78ef5e95bfe87109f651
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-20 06:30:40 +0530

    remove functions push and splitarr

--- manageFetch.js
+++ manageFetch.js
@@ -7,13 +7,12 @@ const urls = {
   jobs: 'jobstories.json'
 }
 
-const itemList = {}
+const itemList = []
 
 const url = 'https://hacker-news.firebaseio.com/v0/'
 
 for (let i in urls) {
-  //  fetch(`${url}${urls[i]}`).then(toJSON).then(data => { splitItemArr(i, data) }).catch(handleErr)
-  itemList[i] = fetch(`${url}${urls[i]}`).then(toJSON).catch(handleErr)
+  itemList.push({ [i]: fetch(`${url}${urls[i]}`).then(toJSON).catch(handleErr) })
 }
 
 function toJSON (data) {
@@ -26,15 +25,4 @@ function handleErr (err) {
 
 function pushEach (list) {
   // stories.items.push(list)
-}
-
-function getItem (data) {
-  data.map(id => fetch(`${url}item/${id}.json`).then(toJSON))
-}
-
-function splitItemArr (i, data) {
-  itemList[i] = []
-  while (data.length > 0) {
-    itemList[i].push(data.splice(0, 20))
-  }
 }
