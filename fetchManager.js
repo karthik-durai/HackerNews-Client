0e89064c7760d41f6145549c4b07bb9329d5c60f
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-24 17:56:19 +0530

    segregate list, fetch items, place it in order

--- fetchManager.js
+++ fetchManager.js
@@ -1,3 +1,5 @@
+const workerSegList = new SharedWorker('workerSegregateList.js')
+
 const urls = {
   top: 'topstories.json',
   new: 'newstories.json',
@@ -10,15 +12,34 @@ const urls = {
 const url = 'https://hacker-news.firebaseio.com/v0/'
 
 let fetchedItems = {}
+let segregatedList = []
+let items = {}
 
 for (let i in urls) {
   fetchedItems[i] = fetch(`${url}${urls[i]}`).then(toJSON)
 }
 
+//  Promise.all(Object.values(fetchedItems)).then(sendToSegregate)
+
+fetchedItems['top'].then(segregrateArray)
+
 function toJSON (data) {
   return data.json()
 }
 
-//  fetch(`${url}${urls['top']}`).then(toJSON).then(console.log)
+function segregrateArray (list) {
+  segregatedList = list.slice(0, 15)
+  fetchItem(segregatedList)
+}
 
-fetchedItems['top'].then(console.log)
+/*  function sendToSegregate (itemList) {
+} */
+
+function fetchItem (list) {
+  for (let i of list) {
+    console.log(i)
+    //  items.push(fetch(`${url}item/${i}.json`).then(toJSON))
+    fetch(`${url}item/${i}.json`).then(toJSON).then(data => { items[list.indexOf(data.id)] = data })
+  }
+  console.log(items)
+}
