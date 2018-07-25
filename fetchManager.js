ab61d8781e26d3a80af1ca199b053f418bc7b42f
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-25 10:35:50 +0530

    able to send message to worker

--- fetchManager.js
+++ fetchManager.js
@@ -1,4 +1,4 @@
-const workerSegList = new SharedWorker('workerSegregateList.js')
+const workerSegList = new Worker('workerSegregateList.js')
 
 const urls = {
   tops: 'topstories.json',
@@ -15,15 +15,11 @@ let fetchedItems = []
 let segregatedList = []
 let items = []
 
+fetch(`${url}${urls['tops']}`).then(toJSON).then(segregrateArray)
+
 for (let i in urls) {
-  fetchedItems.push({[i]: fetch(`${url}${urls[i]}`).then(toJSON)})
-  //  fetch(`${url}${urls[i]}`).then(toJSON).then(data => { workerSegList.port.postMessage([i, data]) })
+  fetch(`${url}${urls[i]}`).then(toJSON).then(data => { workerSegList.postMessage({[i]: data}) })
 }
-
-Promise.all(fetchedItems).then(sendToWorker)
-//  Promise.all(fetchedItems).then(data => console.log(data[0]['top']))
-
-//  fetchedItems['top'].then(segregrateArray)
 
 function toJSON (data) {
   return data.json()
@@ -34,20 +30,8 @@ function segregrateArray (list) {
   fetchItem(segregatedList)
 }
 
-function sendToWorker (itemList) {
-  //  console.log(itemList)
-  let itemObj = []
-  for (let i of itemList) {
-    Object.values(i)[0].then(data => { itemObj[Object.keys(i)[0]] = data })
-  }
-  console.log(itemObj)
-  workerSegList.port.postMessage(itemObj)
-}
-
 function fetchItem (list) {
   for (let i of list) {
-    console.log(i)
-    //  items.push(fetch(`${url}item/${i}.json`).then(toJSON))
     fetch(`${url}item/${i}.json`).then(toJSON).then(data => { items[list.indexOf(data.id)] = data })
   }
   console.log(items)
