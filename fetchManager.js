013af971efaefff8d88489bd1434b6754b544895
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-25 15:31:06 +0530

    got segregated list of items from worker

--- fetchManager.js
+++ fetchManager.js
@@ -1,4 +1,4 @@
-const workerSegList = new Worker('workerSegregateList.js')
+const workerSegregateList = new Worker('workerSegregateList.js')
 
 const urls = {
   tops: 'topstories.json',
@@ -11,28 +11,46 @@ const urls = {
 
 const url = 'https://hacker-news.firebaseio.com/v0/'
 
+workerSegregateList.postMessage(['url', url])
+
+workerSegregateList.onmessage = handleMessages
+
 let fetchedItems = []
-let segregatedList = []
-let items = []
+let segregatedList = {}
+let items = {}
 
-fetch(`${url}${urls['tops']}`).then(toJSON).then(segregrateArray)
+for (let story in urls) {
+  segregatedList[story] = []
+  items[story] = []
+}
 
-for (let i in urls) {
-  fetch(`${url}${urls[i]}`).then(toJSON).then(data => { workerSegList.postMessage({[i]: data}) })
+fetch(`${url}${urls['tops']}`).then(toJSON).then(segregateArray)
+
+for (let story in urls) {
+  fetch(`${url}${urls[story]}`).then(toJSON).then(data => { workerSegregateList.postMessage([story, data]) })
 }
 
 function toJSON (data) {
   return data.json()
 }
 
-function segregrateArray (list) {
-  segregatedList = list.slice(0, 15)
-  fetchItem(segregatedList)
+function segregateArray (list) {
+  fetchItem('top', 0, list.slice(0, 15))
 }
 
-function fetchItem (list) {
-  for (let i of list) {
-    fetch(`${url}item/${i}.json`).then(toJSON).then(data => { items[list.indexOf(data.id)] = data })
+function fetchItem (story, index, list) {
+  for (let id of list) {
+    fetch(`${url}item/${id}.json`).then(toJSON).then(data => { items[story][list.indexOf(data.id)] = data })
   }
   console.log(items)
+}
+
+function handleMessages (e) {
+  if (e.data[0] === 'list') {
+    segregatedList[e.data[1]][e.data[2]] = e.data[3]
+  }
+}
+
+function getItems (story, index) {
+
 }
