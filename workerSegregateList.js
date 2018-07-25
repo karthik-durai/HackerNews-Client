a898188deca2990519ba7f75c8d1d64650f5a09b
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-25 15:31:44 +0530

    segregate and sends to fetchManage

--- workerSegregateList.js
+++ workerSegregateList.js
@@ -1,16 +1,26 @@
-onmessage = (e) => {
-  segregrateArray(e.data)
-}
+onmessage = handleMessage
 
-let segregratedList = {}
+let url
+let segregatedList = {}
 
-function segregrateArray (listObj) {
-  let i = Object.keys(listObj)[0]
-  let list = Object.values(listObj)[0]
-  segregratedList[i] = []
-  while (list.length > 0) {
-    segregratedList[i].push(list.splice(0, 15))
+function handleMessage (e) {
+  if (e.data[0] === 'url') {
+    url = e.data[1]
+  } else {
+    segregateArray(e.data)
   }
-  console.log(segregratedList)
 }
 
+function segregateArray (listObj) {
+  let tmpArr
+  let story = listObj[0]
+  let list = listObj[1]
+  segregatedList[story] = []
+  let index = 0
+  while (list.length > 0) {
+    segregatedList[story].push(list.splice(0, 15))
+    postMessage(['list', story, index, segregatedList[story][index]])
+    index++
+  }
+  //  console.log(segregratedList[story])
+}
