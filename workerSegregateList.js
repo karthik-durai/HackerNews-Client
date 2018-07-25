8d23f283bdc01b377aa5b4331b011aa0e08760bd
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-25 10:36:10 +0530

    can segregrate array

--- workerSegregateList.js
+++ workerSegregateList.js
@@ -1,5 +1,16 @@
-onconnect = (e) => {
-  e.ports[0].onmessage = (e) => {
-    console.log(e.data)
-  }
+onmessage = (e) => {
+  segregrateArray(e.data)
 }
+
+let segregratedList = {}
+
+function segregrateArray (listObj) {
+  let i = Object.keys(listObj)[0]
+  let list = Object.values(listObj)[0]
+  segregratedList[i] = []
+  while (list.length > 0) {
+    segregratedList[i].push(list.splice(0, 15))
+  }
+  console.log(segregratedList)
+}
+
