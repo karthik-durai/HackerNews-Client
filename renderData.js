bf55409c968a189a089a424e11ae9bbee31d16d3
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-26 12:05:19 +0530

    can get type of story using click event

--- renderData.js
+++ renderData.js
@@ -1,13 +1,19 @@
 const appOptions = {
   el: '#app',
   data: {
-    stories: ['top', 'new', 'best', 'ask', 'show', 'jobs']
+    stories: ['top', 'new', 'best', 'ask', 'show', 'jobs'],
+    activeStory: 'top'
+  },
+  methods: {
+    setstory: function (e) {
+      this.activeStory = e.target.textContent
+    }
   }
 }
 
 const navBarOptions = {
   props: ['story'],
-  template: `<ul><li>{{ story | uppercase }}</li></ul>`,
+  template: `<ul><li><a href="#" v-on:click="$emit('setstory', $event)">{{ story | uppercase }}</a></li></ul>`,
   filters: {
     'uppercase': function (text) {
       return text.toUpperCase()
@@ -16,5 +22,6 @@ const navBarOptions = {
 }
 
 Vue.component('nav-bar', navBarOptions)
+//  Vue.component('items-container', itemsContainerOptions)
 
 const app = new Vue(appOptions)
