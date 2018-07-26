d6b39c0e50980b903c87123af8c61ad80b722371
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-26 16:57:30 +0530

    itemList data property is populated only after fetching inistal items

--- fetchManager.js
+++ fetchManager.js
@@ -47,7 +47,7 @@ function fetchItem (story, index, list) {
     fetch(`${url}item/${id}.json`).then(toJSON).then(data => { tempArray[list.indexOf(data.id)] = data })
   }
   items[story][index] = tempArray
-  console.log(items[story][index])
+  //  console.log(items[story][index])
   return items[story][index]
 }
 
@@ -55,10 +55,8 @@ function handleMessages (e) {
   if (e.data[0] === 'list') {
     segregatedList[e.data[1]][e.data[2]] = e.data[3]
   } else if (e.data[0] === 'update') {
-    console.log(e.data[3])
+    //  console.log(e.data[3])
     fetchUpdatedItems(e.data)
-  } else if (e.data[0] === 'noChanges') {
-    console.log('no changes')
   }
 }
 
@@ -67,7 +65,7 @@ function getItems (story, index, list = segregatedList) {
   workerUpdateList.postMessage([story, index, list[story][index]])
   let stories = items[story][index]
   if (stories) {
-    console.log(stories)
+    //  console.log(stories)
     return stories
   } else {
     return fetchItem(story, index, list[story][index])
@@ -89,6 +87,14 @@ function fetchUpdatedItems ([message, story, index, difference]) {
   //  intervalId = setInterval(periodicCheck, 10000, story, index)
 }
 
-function periodicCheck (story, index, list = segregatedList) {
+/*  function periodicCheck (story, index, list = segregatedList) {
   workerUpdateList.postMessage([story, index, list[story][index]])
+} */
+
+function checkIfFetched () {
+  if (items['top'].length > 0) {
+    return 'loaded'
+  } else {
+    return 'not loaded'
+  }
 }
