a8997dcd1938236ba8a133f0d3d4c793da152d4a
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-26 13:18:18 +0530

    added data properties like page number and method to increment or decrement page number

--- renderData.js
+++ renderData.js
@@ -1,17 +1,31 @@
 const appOptions = {
   el: '#app',
+  //  mounted: () => { this.storyList = items['tops'][0] },
   data: {
     stories: ['top', 'new', 'best', 'ask', 'show', 'jobs'],
-    activeStory: 'top'
+    directions: ['prev', 'next'],
+    activeStory: 'top',
+    pageNumber: 1
   },
   methods: {
     setstory: function (e) {
-      this.activeStory = e.target.textContent
+      this.activeStory = e.target.textContent.toLowerCase()
+      getItems(this.activeStory, 0)
+    },
+    setpagenum: function (e) {
+      let text = e.target.textContent
+      if (text === 'prev') {
+        this.pageNumber--
+        console.log(this.pageNumber)
+      } else if (text === 'next') {
+        this.pageNumber++
+        console.log(this.pageNumber)
+      }
     }
   }
 }
 
-const navBarOptions = {
+const storiesNavOptions = {
   props: ['story'],
   template: `<ul><li><a href="#" v-on:click="$emit('setstory', $event)">{{ story | uppercase }}</a></li></ul>`,
   filters: {
@@ -21,7 +35,18 @@ const navBarOptions = {
   }
 }
 
-Vue.component('nav-bar', navBarOptions)
+const pageNavOptions = {
+  props: ['direction'],
+  template: `<button v-on:click="$emit('setpagenum', $event)">{{ direction }}</button>`
+}
+
+const itemsContainerOptions = {
+  props: ['item'],
+  template: `<ul><li>{{ item.title }}</li></ul>`
+}
+
+Vue.component('stories-nav', storiesNavOptions)
+Vue.component('page-nav', pageNavOptions)
 //  Vue.component('items-container', itemsContainerOptions)
 
 const app = new Vue(appOptions)
