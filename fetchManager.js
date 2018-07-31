dee9d24dcca780597f73ef9c6c2599e5a267fe16
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-31 11:30:12 +0530

    put sensible names, catch fetch api error

--- fetchManager.js
+++ fetchManager.js
@@ -17,68 +17,50 @@ workerUpdateList.postMessage(['url', url, urls])
 workerSegregateList.onmessage = handleMessages
 workerUpdateList.onmessage = handleMessages
 
+let items = {}
+let segregatedList = {}
+
 
 for (let story in urls) {
   segregatedList[story] = []
   items[story] = []
 }
 
-fetch(`${url}${urls['top']}`).then(toJSON).then(segregateArray)
+fetch(`${url}${urls['top']}`).then(toJSON).then(getTopFifteen).catch(console.log)
 
-for (let story in urls) {
-  fetch(`${url}${urls[story]}`).then(toJSON).then(data => { workerSegregateList.postMessage([story, data]) })
+for (let storyType in urls) {
+  fetch(`${url}${urls[storyType]}`).then(toJSON).then(list => { workerSegregateList.postMessage([storyType, list]) }).catch(console.log)
 }
 
 function toJSON (data) {
   return data.json()
 }
 
-function segregateArray (list) {
-  fetchItem('top', 0, list.slice(0, 15))
+function getTopFifteen (list) {
+  fetchEachItem('top', 0, list.slice(0, 15))
 }
 
-function fetchItem (story, index, list) {
-  items[story][index] = []
+function fetchEachItem (storyType, index, list) {
+  items[storyType][index] = []
   for (let id of list) {
-    items[story][index].push(fetch(`${url}item/${id}.json`).then(toJSON))
+    items[storyType][index].push(fetch(`${url}item/${id}.json`).then(toJSON))
   }
 }
 
 function handleMessages (e) {
   if (e.data[0] === 'list') {
     segregatedList[e.data[1]][e.data[2]] = e.data[3]
-  } else if (e.data[0] === 'update') {
-    fetchUpdatedItems(e.data)
+  } else if (e.data[0] === 'updatedList') {
+    console.log(e.data)
   }
 }
 
-function getItems (story, index, list = segregatedList) {
-  workerUpdateList.postMessage([story, index, list[story][index]])
-  let stories = items[story][index]
+function getItems (storyType, index, list = segregatedList) {
+  workerUpdateList.postMessage(['updateList', storyType, index, list[storyType][index]])
+  let stories = items[storyType][index]
   if (stories) {
     return stories
   } else {
-    fetchItem(story, index, list[story][index])
+    fetchEachItem(storyType, index, list[storyType][index])
   }
-}
-
-function fetchUpdatedItems ([_, story, index, difference]) {
-  let indexList = Object.keys(difference)
-  let idList = Object.values(difference)
-  console.log(idList)
-  if (indexList.length > 0) {
-    for (let i in idList) {
-      items[story][index].splice(indexList[i], 1, fetch(`{url}item/${idList[i]}.json`).then(toJSON))
-    }
-  }
-}
-
-function getComments (story, index, storyId) {
-  if (comments[storyId]) { return }
-  comments[storyId] = []
-  items[story][index].forEach(i => i.then(data => {
-    if (storyId === data.id) {
-      data.kids.forEach(i => comments[storyId].push(fetch(`${url}item/${i}.json`).then(toJSON)))
-    }
-  }))
 }
