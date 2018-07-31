70762beac305828e7cf724fce21fca78ddb1cb3b
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-07-31 16:59:16 +0530

    renders stories in correct order

--- render.js
+++ render.js
@@ -2,8 +2,42 @@ const appOptions = {
   el: '#app',
   data: {
     stories: {},
-    segregatedList: {}
+    segregatedList: {},
+    activeStoryType: 'top',
+    activePageNumber: 1,
+    toBeRendered: [],
+    interval: ''
+  },
+  methods: {
+    checkIfLoaded: checkIfLoaded,
+    populate: populateStories
+  },
+  mounted: function () {
+    this.interval = setInterval(this.checkIfLoaded, 1000)
+  },
+  watch: {
+    activePageNumber: function () {
+      fetchItems(this.activeStoryType, this.activePageNumber - 1)
+      this.checkIfLoaded()
+    },
+    activeStoryType: function () {
+      this.activePageNumber = 1
+      fetchItems(this.activeStoryType, this.activePageNumber - 1)
+    }
   }
+}
+
+function checkIfLoaded () {
+  let st = this.activeStoryType
+  let i = this.activePageNumber - 1
+  if (this.stories[st][i]) {
+    clearInterval(this.interval)
+    this.stories[st][i].forEach((item) => { this.populate(st, i, item) })
+  }
+}
+
+function populateStories (st, i, item) {
+  item.then(story => { this.toBeRendered.splice(this.stories[st][i].indexOf(item), 1, story) })
 }
 
 const app = new Vue(appOptions)
